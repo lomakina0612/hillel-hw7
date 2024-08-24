542e176da2bc850bf9bd40be40af1d478bf37eb5
Elena Lomakina <lomakina0612@gmail.com>
2024-08-25 00:25:57 +0200

    create an .xls file with data

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import json
 import requests
+import openpyxl
+
+from openpyxl import Workbook
+
 
 rates =[]
 for year in range(2021, 2022):
-   for month in range(1,13):
+   for month in range(1,5):
       r = requests.get(f'https://api.privatbank.ua/p24api/exchange_rates?json&date=01.{month:02}.{year}')
       
       if r.status_code == 200:
@@ -14,3 +18,15 @@ for year in range(2021, 2022):
       print(f'01.{month:02}.{year} 1 EUR = {rate_eur[0]['saleRate']:.2f} UAH') 
       rates.append((f'01.{month:02}.{year}',rate_eur[0]['saleRate']))
    print(rates)
+   
+wb = Workbook()
+ws = wb.active
+ws.title = "EUR exchange rate"
+ws['A1'] = 'Date'
+ws['B1'] = 'EUR'
+
+for idx, rate in enumerate(rates):
+   ws[f'A{idx+2}'] = rate[0]
+   ws[f'B{idx+2}'] = rate[1]
+
+wb.save("rates.xlsx")
